b4e4144f50764981d70786cfbc91660bb03c5249
Andrew <andrew.chen986@gmail.com>
2022-10-02 15:40:21 -0500

    Add color changing grid

--- sketch.js
+++ sketch.js
@@ -1,16 +1,23 @@
 // make 16x16 grid
 
-container = document.querySelector('#container');
+const container = document.querySelector('#container');
 
 for (let i = 0; i < 16; i++) {
     // create a row
-    row = document.createElement('div');
+    const row = document.createElement('div');
     row.setAttribute('class','row');
     for (let j = 0; j < 16; j++) {
-        col = document.createElement('div');
+        const col = document.createElement('div');
         col.setAttribute('class','col');
-        col.textContent = `${j}`
         row.appendChild(col);
     }
     container.appendChild(row);
 }
+
+// event listeners to change background-color on hover
+const gridSquares = document.querySelectorAll('.col');
+gridSquares.forEach(gridSquare => {
+    gridSquare.addEventListener('mouseover', () => {
+        gridSquare.classList.add('hovered')
+    });
+});
